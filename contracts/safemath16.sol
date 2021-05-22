e348c15d4bc5e2cd037b9a237efb7edf7b5decfa
Burak ARAT <burak_arat@hotmail.com>
2021-05-23 00:02:58 +0300

    first-commit

---
+++ contracts/safemath16.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+library SafeMath16 {
+
+  function mul(uint16 a, uint16 b) internal pure returns (uint16) {
+    if (a == 0) {
+      return 0;
+    }
+    uint16 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint16 a, uint16 b) internal pure returns (uint16) {
+    uint16 c = a / b;
+    return c;
+  }
+
+  function sub(uint16 a, uint16 b) internal pure returns (uint16) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint16 a, uint16 b) internal pure returns (uint16) {
+    uint16 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
---
+++ contracts/safemath32.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+library SafeMath32 {
+
+  function mul(uint32 a, uint32 b) internal pure returns (uint32) {
+    if (a == 0) {
+      return 0;
+    }
+    uint32 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint32 a, uint32 b) internal pure returns (uint32) {
+    uint32 c = a / b;
+    return c;
+  }
+
+  function sub(uint32 a, uint32 b) internal pure returns (uint32) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint32 a, uint32 b) internal pure returns (uint32) {
+    uint32 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
